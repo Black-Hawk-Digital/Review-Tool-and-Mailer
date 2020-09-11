8552a2cdf62e9bdef8061d52548f417def9fce40
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-09-11 11:39:09 -0400

    passwords and secrets removed

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: 'yourgoogleclientID',
+    googleClientSecret: 'yourgoogleclientsecret',
+    mongoURI:'yourmongoconnection',
+    cookieKey:'anyrandomstring',
+    redirectDomain: 'http://localhost:3000',
+    emailFrom: 'youremail'
+  };
